dfe1debad3262e9f2825395b0031625946cee19b
Benjamin Bayart <bbayart@ext.scaleway.com>
2024-05-07 15:12:43 +0200

    Added config

---
+++ src/config.py
@@ -0,0 +1,10 @@
+from dynaconf import Dynaconf
+
+settings = Dynaconf(
+    envvar_prefix="DIMAIL",
+    settings_files=['settings.toml', '.secrets.toml'],
+    ignore_unknown_envvars=True,
+)
+
+# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+# `settings_files` = Load these files in the order.
