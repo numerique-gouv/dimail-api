6078d2fd3da827d77c3adea6439a2271b0516de1
Raphaël Squelbut <raphael.squelbut@shodo.io>
2024-05-15 22:58:19 +0200

    vérifie que l'environnement python est configuré et activé

--- start-dev.sh
+++ start-dev.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-DOCKER=docker
+# vérifie que python est bien activé
+if ! command -v alembic > /dev/null 2>&1 || ! command -v uvicorn > /dev/null 2>&1; then
+  echo "l'environnement python n'est pas configuré ou est désactivé (voir le README)"
+  exit 1
+fi
 
+# défini le runtime de container
+DOCKER=docker
 if command -v podman > /dev/null 2>&1; then
   # Vérifie si la commande docker est disponible et fonctionne
   if ! command -v docker > /dev/null 2>&1 || ! docker ps > /dev/null 2>&1; then
