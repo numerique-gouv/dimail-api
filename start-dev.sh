f54ce10d87fe6851b572bd447ebe82ec5241add1
Raphaël Squelbut <raphael.squelbut@shodo.io>
2024-05-15 22:29:26 +0200

    permet d'utiliser podman à la place de docker si docker n'est pas lancé et podman est présent

--- start-dev.sh
+++ start-dev.sh
@@ -1,12 +1,22 @@
-if command -v docker &> /dev/null; then DOCKER=docker; else DOCKER=podman;fi
+#!/bin/bash
+
+DOCKER=docker
+
+if command -v podman > /dev/null 2>&1; then
+  # Vérifie si la commande docker est disponible et fonctionne
+  if ! command -v docker > /dev/null 2>&1 || ! docker ps > /dev/null 2>&1; then
+    echo "podman sera utilisé à la place de docker"
+    DOCKER=podman
+  fi
+fi
 
 $DOCKER compose up -d
 sleep 25
-cd src/
+cd src/ || exit
 alembic upgrade head
 cd ..
-uvicorn src.main:app --reload
-cd src/
+uvicorn src.main:app --reload --log-level info
+cd src/ || exit
 # alembic downgrade head
 cd ..
 # $DOCKER compose down
