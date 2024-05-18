1349715cb7bd2b9c994c3df8f5105e8d08a51611
Benjamin Bayart <bbayart@ext.scaleway.com>
2024-05-18 10:50:41 +0200

    Fix tests for routes

--- src/routes/test_basic.py
+++ src/routes/test_basic.py
@@ -13,11 +13,31 @@ def my_user(db_api_maker, log):
     user = "bidibule"
     domain = "tutu.net"
 
-    client.post("/admin/users", json={"name": user, "is_admin": False})
-    client.post(
-        "/admin/domains", json={"name": domain, "features": ["webmail", "mailbox"]}
+    res = client.post(
+        "/admin/users",
+        json={
+            "name": user,
+            "password": "toto",
+            "is_admin": False
+        }
     )
-    client.post("/admin/allows", json={"user": user, "domain": domain})
+    assert res.status_code == 200
+    res = client.post(
+        "/admin/domains",
+        json={
+            "name": domain,
+            "features": ["webmail", "mailbox"]
+        }
+    )
+    assert res.status_code == 200
+    client.post(
+        "/admin/allows",
+        json={
+            "user": user,
+            "domain": domain
+        }
+    )
+    assert res.status_code == 200
 
     yield {"user": user, "domains": [domain]}
 
