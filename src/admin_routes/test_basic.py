a3440ae4e57d6ac1addfabfec7483f44be62f6fc
Benjamin Bayart <bbayart@ext.scaleway.com>
2024-05-18 10:43:56 +0200

    Mise a jour des tests d'admin_routes

--- src/admin_routes/test_basic.py
+++ src/admin_routes/test_basic.py
@@ -11,12 +11,14 @@ def test_something(db_api_maker, log):
     response = client.get("/admin/users")
     assert response.status_code == 200
     assert response.json() == []
-    response = client.post("/admin/users", json={"name": "testing", "is_admin": False})
+    response = client.post("/admin/users", json={"name": "testing", "password": "toto", "is_admin": False})
     assert response.status_code == 200
-    assert response.json() == {"name": "testing", "is_admin": False}
+    # TODO : Comment piloter la génération des uuid depuis les tests pour les rendre prédictibles ?
+    uuid = response.json()["uuid"]
+    assert response.json() == {"name": "testing", "uuid": uuid, "is_admin": False}
     response = client.get("/admin/users")
     assert response.status_code == 200
-    assert response.json() == [{"name": "testing", "is_admin": False}]
-    response = client.post("/admin/users", json={"name": "testing", "is_admin": False})
+    assert response.json() == [{"name": "testing", "uuid": uuid, "is_admin": False}]
+    response = client.post("/admin/users", json={"name": "testing", "password": "titi", "is_admin": False})
     assert response.status_code == 409
     assert response.json() == {"detail": "User already exists"}
