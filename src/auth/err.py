caae6544d575046ed8c4c377ed42a83f3c8971fb
Raphaël Squelbut <raphael.squelbut@shodo.io>
2024-05-21 11:54:50 +0200

    retire le mot clé `return` dans la méthode `__init__`

--- src/auth/err.py
+++ src/auth/err.py
@@ -7,7 +7,7 @@ class PermissionDenied(fastapi.HTTPException):
     def __init__(self):
         log = logging.getLogger(__name__)
         log.debug("Our own way to tell you you are not welcome")
-        return super(PermissionDenied, self).__init__(
+        super(PermissionDenied, self).__init__(
             status_code=fastapi.status.HTTP_401_UNAUTHORIZED,
             detail="Permission denied",
         )
