b18b1fe5061032716e13c0e48631190355b5bb97
Raphaël Squelbut <raphael.squelbut@shodo.io>
2024-05-21 10:42:47 +0200

    affiche le runtime de container qui sera utilisé par le script

--- start-dev.sh
+++ start-dev.sh
@@ -11,10 +11,11 @@ DOCKER=docker
 if command -v podman > /dev/null 2>&1; then
   # Vérifie si la commande docker est disponible et fonctionne
   if ! command -v docker > /dev/null 2>&1 || ! docker ps > /dev/null 2>&1; then
-    echo "podman sera utilisé à la place de docker"
     DOCKER=podman
   fi
 fi
+
+echo "container runtime -> ${DOCKER}"
 
 $DOCKER compose up -d
 sleep 25
