602cc51bc8035d86ebcd28d6d5baa0f2fea19701
Benjamin Bayart <bbayart@ext.scaleway.com>
2024-05-28 12:03:33 +0200

    Added an utils module, with split_email

---
+++ src/utils/mail.py
@@ -0,0 +1,16 @@
+import re
+
+mail_re = re.compile("^(?P<username>[^@]+)@(?P<domain>[^@]+)$")
+
+def split_email(email: str) -> tuple:
+    test_mail = mail_re.match(email)
+    if test_mail is None:
+        raise Exception(f"The email address <{email}> is not valid")
+
+    infos = test_mail.groupdict()
+    domain = infos["domain"]
+    username = infos["username"]
+
+    return (username, domain)
+
+
