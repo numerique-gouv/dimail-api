cf96434f5b5d24856cfcf8ade91610490b5a3da0
Benjamin Bayart <130998887+octo-yart@users.noreply.github.com>
2024-06-19 13:38:20 +0200

    Catch the lost connexions and give an EXPLICIT reference to where it … (#10)
    * Catch the lost connexions and give an EXPLICIT reference to where it was built
    
    ---------
    
    Co-authored-by: Benjamin Bayart <bbayart@ext.scaleway.com>

--- src/sql_api/database.py
+++ src/sql_api/database.py
@@ -1,4 +1,5 @@
 import atexit
+import inspect
 
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
@@ -17,20 +18,25 @@ def init_db(config: str):
     global maker
     url = config
     engine = sa.create_engine(url)
-    maker = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    maker = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine, info={}, close_resets_only=False)
 
 
 def get_db():
     global db
+    raise Exception("Deprecated")
     if db is None:
         maker = get_maker()
         db = maker()
         atexit.register(lambda: db.close())
     return db
 
-
 def get_maker():
     global maker
     if maker is None:
         raise Exception("You need to init the db by giving me a valid URL")
+    caller = inspect.currentframe().f_back.f_code.co_qualname
+    file = inspect.currentframe().f_back.f_code.co_filename
+    line = inspect.currentframe().f_back.f_code.co_firstlineno
+    maker.kw["info"]["caller"] = caller
+    maker.kw["info"]["file"] = f"{file}:{line}"
     return maker
