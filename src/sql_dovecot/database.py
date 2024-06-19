b16102bdfd10d56c9bc912bc7e58108bc2827ee4
Benjamin Bayart <bbayart@ext.scaleway.com>
2024-06-19 13:37:56 +0200

    Clean useless key in the session infos

--- src/sql_dovecot/database.py
+++ src/sql_dovecot/database.py
@@ -22,7 +22,7 @@ def init_db(config: str):
     global maker
     url = config
     engine = sa.create_engine(url)
-    maker = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine, info={"key": "Benjamin"}, close_resets_only=False)
+    maker = orm.sessionmaker(autocommit=False, autoflush=False, bind=engine, info={}, close_resets_only=False)
 
 def get_maker() -> orm.sessionmaker:
     global maker
@@ -31,7 +31,6 @@ def get_maker() -> orm.sessionmaker:
     caller = inspect.currentframe().f_back.f_code.co_qualname
     file = inspect.currentframe().f_back.f_code.co_filename
     line = inspect.currentframe().f_back.f_code.co_firstlineno
-    maker.kw["info"]["key"] = "Lautre"
     maker.kw["info"]["caller"] = caller
     maker.kw["info"]["file"] = f"{file}:{line}"
     return maker
