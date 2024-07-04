ee69f80114d32d4a831295d3e5bf741f2a091a52
Benjamin Bayart <bbayart@ext.scaleway.com>
2024-07-04 12:38:47 +0200

    Stupid code for useless tools

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -3,6 +3,15 @@ from . import aliases, allows, domains, mailboxes, users
 
 from .get_token import login_for_access_token
 
+# Stupid useless variable to shut up the linter
+__useless__ = [
+    aliases.get_alias,
+    allows.get_allows,
+    domains.get_domain,
+    mailboxes.get_mailboxes,
+    users.get_user,
+]
+
 __all__ = [
     login_for_access_token,
 ]
