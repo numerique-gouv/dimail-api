cafd8714779b7e30152d099c69fff4a77eb7978a
Benjamin Bayart <bbayart@ext.scaleway.com>
2024-07-05 10:14:23 +0200

    Remove dead functions

--- src/sql_dovecot/__init__.py
+++ src/sql_dovecot/__init__.py
@@ -1,12 +1,11 @@
 from .crud import create_user, get_user, get_users
-from .database import get_db, get_maker, init_db
+from .database import get_maker, init_db
 from .models import ImapUser
 
 __all__ = [
     create_user,
     get_users,
     get_user,
-    get_db,
     get_maker,
     init_db,
     ImapUser,
--- src/sql_postfix/database.py
+++ src/sql_postfix/database.py
@@ -5,13 +5,8 @@ import sqlalchemy as sa
 import sqlalchemy.orm as orm
 
 maker: orm.sessionmaker | None = None
-db: orm.Session
 
 Postfix = orm.declarative_base()
-
-
-def close_db(db):
-    db.close()
 
 
 def init_db(config: str):
@@ -36,11 +31,3 @@ def get_maker() -> orm.sessionmaker:
     maker.kw["info"]["file"] = f"{file}:{line}"
     return maker
 
-
-def get_db() -> orm.Session:
-    global db
-    if db is None:
-        maker = get_maker()
-        db = maker()
-        atexit.register(lambda: db.close())
-    return db
