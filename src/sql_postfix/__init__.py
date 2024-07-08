4e21da5cdad1d6849c8f973c130d011420ba40fa
Benjamin Bayart <bbayart@ext.scaleway.com>
2024-07-08 15:59:02 +0200

    Useless commit to please pre-commit

--- src/sql_postfix/__init__.py
+++ src/sql_postfix/__init__.py
@@ -1,4 +1,11 @@
-from .crud import create_alias, delete_alias, delete_aliases_by_name, get_alias, get_aliases_by_domain, get_aliases_by_name
+from .crud import (
+    create_alias,
+    delete_alias,
+    delete_aliases_by_name,
+    get_alias,
+    get_aliases_by_domain,
+    get_aliases_by_name
+)
 from .database import get_maker, init_db
 from .models import PostfixAlias
 
