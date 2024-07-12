9ecf725cf7dc8471d72d92ed7b84ad4728812444
Hugues DUMONT <hugues.dumont@octo.com>
2024-07-12 11:44:17 +0200

    test(crud dovecot): Unit tests on dovecot crud

--- src/sql_dovecot/test_basic.py
+++ src/sql_dovecot/test_basic.py
@@ -6,6 +6,19 @@ def test_imap__create_and_get_a_user(db_dovecot_session):
     imap_user = sql_dovecot.get_user(db_dovecot_session, "toto", "example.com")
     assert imap_user is None
 
+    # On essaye de créer un user avec des valeurs idiotes -> return None
+    imap_user = sql_dovecot.create_user(db_dovecot_session, None, None, None)
+    assert imap_user is None
+
+    # Le test précédent a échoué à cause du mot de passe incohérent
+    # Si une ou plusieurs autre(s) valeur(s) est incohérente, le résultat sera le même.
+    imap_user = sql_dovecot.create_user(db_dovecot_session, None, None, "secret")
+    assert imap_user is None
+    imap_user = sql_dovecot.create_user(db_dovecot_session, None, "example.com", "secret")
+    assert imap_user is None
+    imap_user = sql_dovecot.create_user(db_dovecot_session, "toto", None, "secret")
+    assert imap_user is None
+
     imap_user = sql_dovecot.create_user(
         db_dovecot_session, "toto", "example.com", "secret"
     )
@@ -26,3 +39,38 @@ def test_imap__create_and_get_a_user(db_dovecot_session):
     # Après qu'on l'a supprimé, le user n'existe plus.
     imap_user = sql_dovecot.get_user(db_dovecot_session, "toto", "example.com")
     assert imap_user is None
+
+def test_delete_user(db_dovecot_session):
+    # On essaye de supprimer un user qui n'existe pas
+    user = sql_dovecot.delete_user(db_dovecot_session, "new", "example.com")
+    assert user is None
+
+    # On crée un user pour le test
+    user = sql_dovecot.create_user(db_dovecot_session, "new", "example.com", "secret")
+    assert user is not None
+
+    # On supprime le user créé
+    user = sql_dovecot.delete_user(db_dovecot_session, "new", "example.com")
+    assert user is not None
+
+def test_get_users(db_dovecot_session):
+    # On teste la récupération des users d'un domaine inexistant
+    # Mais ce domaine n'a encore aucun user pour le moment
+    users = sql_dovecot.get_users(db_dovecot_session, "example.com")
+    assert users == []
+
+    # On ajoute un user dans le domaine
+    user = sql_dovecot.create_user(db_dovecot_session, "new", "example.com", "secret")
+    assert user is not None
+
+    # On teste la récupération des users du domaine
+    users = sql_dovecot.get_users(db_dovecot_session, "example.com")
+    assert len(users) == 1
+
+    # On ajoute un second user dans le domaine
+    user = sql_dovecot.create_user(db_dovecot_session, "new2", "example.com", "secret")
+    assert user is not None
+
+    # On teste la récupération des users du domaine
+    users = sql_dovecot.get_users(db_dovecot_session, "example.com")
+    assert len(users) == 2
