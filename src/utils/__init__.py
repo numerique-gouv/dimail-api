31ddb1e52a07e4a2e9b3583ca20abd592ba755d4
Hugues DUMONT <hugues.dumont@octo.com>
2024-07-18 15:34:44 +0200

    docs(utils): technical docs for package

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,14 @@
+"""This module contains utility functions for the project.
+
+Attributes:
+    * __all__: List of all modules in the package.
+
+Modules:
+    * mail: Functions for handling email addresses.
+
+Functions:
+    * split_email: Splitting an email address into username and domain.
+"""
 from .mail import split_email
 
 __all__ = [split_email]
--- src/utils/mail.py
+++ src/utils/mail.py
@@ -1,9 +1,28 @@
+"""This module contains the function to split an email address into its username and domain parts
+
+Variables:
+   - mail_re {re.Pattern} -- The regular expression to match an email address
+
+Functions:
+    - split_email(tuple): Split an email address into its username and domain parts
+"""
 import re
 
 mail_re = re.compile("^(?P<username>[^@]+)@(?P<domain>[^@]+)$")
 
 
 def split_email(email: str) -> tuple:
+    """Split an email address into its username and domain parts
+
+    Args:
+        email (str): The email address to split
+
+    Returns:
+        tuple: A tuple containing the username and domain parts of the email address
+
+    Raises:
+        Exception: If the email address is not valid
+    """
     test_mail = mail_re.match(email)
     if test_mail is None:
         raise Exception(f"The email address <{email}> is not valid")
