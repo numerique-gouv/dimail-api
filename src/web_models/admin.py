b59de1c4a16d5a28b0ae6236e747ef5151310414
Hugues DUMONT <hugues.dumont@octo.com>
2024-07-18 16:25:00 +0200

    docs(web_models): technical docs for package

--- src/web_models/admin.py
+++ src/web_models/admin.py
@@ -1,3 +1,13 @@
+"""This module contains the admin models.
+
+Classes:
+    - Allowed(pydantic.BaseModel): Allowed model.
+    - CreateUser(pydantic.BaseModel): CreateUser model.
+    - Domain(pydantic.BaseModel): Domain model.
+    - Feature(enum.StrEnum): Feature model.
+    - Token(pydantic.BaseModel): Token model.
+    - User(pydantic.BaseModel): User model.
+"""
 import enum
 import uuid
 
@@ -7,23 +17,62 @@ from .. import sql_api
 
 
 class Feature(enum.StrEnum):
+    """Feature enumeration.
+
+    Attributes:
+        Webmail: Webmail feature.
+        Mailbox: Mailbox feature.
+        Alias: Alias feature.
+    """
     Webmail = "webmail"
     Mailbox = "mailbox"
     Alias = "alias"
 
 
 class Token(pydantic.BaseModel):
+    """Token model.
+
+    Attributes:
+        access_token (str): Access token.
+        token_type (str): Token type.
+    """
     access_token: str
     token_type: str
 
 
 class User(pydantic.BaseModel):
+    """User model.
+
+    Attributes:
+        name (str): User name.
+        is_admin (bool): Admin flag.
+        uuid (uuid.UUID): User UUID.
+
+    Methods:
+        from_db(cls, in_db: sql_api.DBUser): Create a User model from a DBUser model.
+    """
     name: str
     is_admin: bool
     uuid: uuid.UUID
 
     @classmethod
     def from_db(cls, in_db: sql_api.DBUser):
+        """Transform a DBUser model into a User model.
+
+        Args:
+            in_db (sql_api.DBUser): DBUser model.
+
+        Returns:
+            User: User model.
+
+        Example:
+
+            >>> user = User.from_db(
+            >>>     DBUser(name="user",
+            >>>     is_admin=False,
+            >>>     uuid=uuid.UUID("12345678-1234-5678-1234-567812345678")))
+            >>> print(user.name)
+        """
         return cls(
             name=in_db.name,
             is_admin=in_db.is_admin,
@@ -32,17 +81,63 @@ class User(pydantic.BaseModel):
 
 
 class CreateUser(pydantic.BaseModel):
+    """CreateUser model.
+
+    Attributes:
+        name (str): User name.
+        password (str): User password.
+        is_admin (bool): Admin flag.
+
+    Example:
+        >>> user = CreateUser(name="user", password="password", is_admin=False)
+        >>> print(user.name)
+    """
     name: str
     password: str
     is_admin: bool
 
 
 class UpdateUser(pydantic.BaseModel):
+    """UpdateUser model.
+
+    Attributes:
+        name (str): User name.
+        password (str): User password.
+        is_admin (bool): Admin flag.
+
+    Example:
+        >>> user = UpdateUser(name="user", password="password", is_admin=False)
+        >>> print(user.name)
+    """
     password: str | None = None
     is_admin: bool | None = None
 
 
 class Domain(pydantic.BaseModel):
+    """Domain model.
+
+    Attributes:
+        name (str): Domain name.
+        features (list[Feature]): List of features.
+        mailbox_domain (str): Mailbox domain.
+        webmail_domain (str): Webmail domain.
+        imap_domains (list[str]): IMAP domains.
+        smtp_domains (list[str]): SMTP domains.
+        context_name (str): Context name.
+
+    Methods:
+        from_db(Domain): Create a Domain model from a DBDomain model.
+
+    Example:
+        >>> domain = Domain(name="example.com",
+        >>>               features=[Feature.Mailbox],
+        >>>               mailbox_domain="example.com",
+        >>>               webmail_domain="webmail.example.com",
+        >>>               imap_domains=["imap.example.com"],
+        >>>               smtp_domains=["smtp.example.com"],
+        >>>               context_name="example")
+        >>> print(domain.name)
+    """
     name: str
     features: list[Feature]
     mailbox_domain: str | None = None
@@ -53,6 +148,26 @@ class Domain(pydantic.BaseModel):
 
     @classmethod
     def from_db(cls, in_db: sql_api.DBDomain, ctx_name: str | None = None):
+        """Transform a DBDomain model into a Domain model.
+
+        Args:
+            in_db (sql_api.DBDomain): DBDomain model.
+            ctx_name (str | None): Context name.
+
+        Returns:
+            Domain: Domain model.
+
+        Example:
+            >>> domain = Domain.from_db(
+            >>>     DBDomain(name="example.com",
+            >>>     features=[Feature.Mailbox],
+            >>>     mailbox_domain="example.com",
+            >>>     webmail_domain="webmail.example.com",
+            >>>     imap_domains=["imap.example.com"],
+            >>>     smtp_domains=["smtp.example.com"]),
+            >>>     ctx_name="example")
+            >>> print(domain.name)
+        """
         return cls(
             name=in_db.name,
             features=in_db.features,
@@ -65,11 +180,37 @@ class Domain(pydantic.BaseModel):
 
 
 class Allowed(pydantic.BaseModel):
+    """Allowed model.
+
+    Attributes:
+        user (str): User name.
+        domain (str): Domain name.
+
+    Methods:
+        from_db(Allowed): Create an Allowed model from a DBAllowed model.
+
+    Example:
+        >>> allowed = Allowed(user="user", domain="example.com")
+        >>> print(allowed.user)
+    """
     user: str
     domain: str
 
     @classmethod
     def from_db(cls, in_db: sql_api.DBAllowed):
+        """Transform a DBAllowed model into an Allowed model.
+
+        Args:
+            in_db (sql_api.DBAllowed): DBAllowed model.
+
+        Returns:
+            Allowed: Allowed model.
+
+        Example:
+            >>> allowed = Allowed.from_db(
+            >>>     DBAllowed(user="user", domain="example.com"))
+            >>> print(allowed.user)
+        """
         return cls(
             user=in_db.user,
             domain=in_db.domain,
--- src/web_models/alias.py
+++ src/web_models/alias.py
@@ -1,15 +1,52 @@
+"""This module contains the Alias class, which is a pydantic model for the Alias object.
+
+Classes:
+    - Alias(pydantic.BaseModel): Alias model.
+    - CreateAlias(pydantic.BaseModel): CreateAlias model.
+"""
 import pydantic
 
 from .. import sql_postfix, utils
 
 
 class Alias(pydantic.BaseModel):
+    """Alias model.
+
+    Attributes:
+        username (str): Username.
+        domain (str): Domain.
+        destination (str): Destination.
+
+    Methods:
+        from_db(Alias): Create an Alias model from a PostfixAlias model.
+    """
     username: str
     domain: str
     destination: str
 
     @classmethod
     def from_db(cls, dbAlias: sql_postfix.PostfixAlias):
+        """Create an Alias model from a PostfixAlias model.
+
+        Args:
+            dbAlias (PostfixAlias): PostfixAlias model.
+
+        Returns:
+            Alias: Alias model.
+
+        Raises:
+            Exception: If the alias in the database is not consistent.
+
+        Example:
+            >>> from src import sql_postfix
+            >>>
+            >>> Alias.from_db(
+            >>>       sql_postfix.PostfixAlias(
+            >>>         alias="user@domain",
+            >>>         destination="user@domain",
+            >>>         domain="domain"))
+            >>> Alias(username="user", domain="domain", destination="user@domain")
+        """
         (username, domain) = utils.split_email(dbAlias.alias)
         if domain != dbAlias.domain:
             raise Exception(
@@ -23,5 +60,15 @@ class Alias(pydantic.BaseModel):
         )
 
 class CreateAlias(pydantic.BaseModel):
+    """CreateAlias model.
+
+    Attributes:
+        user_name (str): User name.
+        destination (str): Destination.
+
+    Example:
+        >>> create_alias = CreateAlias(user_name="user", destination="user@domain")
+        >>> print(create_alias.user_name, create_alias.destination)
+    """
     user_name: str
     destination: str
