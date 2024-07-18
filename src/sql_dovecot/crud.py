a7bba96cb691796290daab3265c3b9ebc7634f10
Hugues DUMONT <hugues.dumont@octo.com>
2024-07-18 14:54:04 +0200

    docs(sql_dovecot): technical docs for package

--- src/sql_dovecot/crud.py
+++ src/sql_dovecot/crud.py
@@ -4,14 +4,46 @@ from . import models
 
 
 def get_user(db: orm.Session, username: str, domain: str):
+    """Get a user by name.
+
+    Args:
+        db: the database session
+        username: the name of the user
+        domain: the domain of the user
+
+    Returns:
+        ImapUser: the user with the given name
+    """
     return db.get(models.ImapUser, {"username": username, "domain": domain})
 
 
 def get_users(db: orm.Session, domain_name: str):
+    """Get all users.
+
+    Args:
+        db: the database session
+        domain_name: the name of the domain
+
+    Returns:
+        list[ImapUser]: a list of all users
+    """
     return db.query(models.ImapUser).filter(models.ImapUser.domain == domain_name).all()
 
 
 def create_user(db: orm.Session, username: str, domain: str, password: str):
+    """Create a user.
+
+    Args:
+        db: the database session
+        username: the name of the user
+        domain: the domain of the user
+        password: the password of the user
+
+    Returns:
+        ImapUser: the created user
+
+        None: if the user could not be created
+    """
     imap_user = models.ImapUser(
         username=username,
         domain=domain,
@@ -34,6 +66,18 @@ def create_user(db: orm.Session, username: str, domain: str, password: str):
 
 
 def delete_user(db: orm.Session, username: str, domain: str):
+    """Delete a user.
+
+    Args:
+        db: the database session
+        username: the name of the user
+        domain: the domain of the user
+
+    Returns:
+        ImapUser: the deleted user
+
+        None: if the user could not be deleted
+    """
     user = get_user(db, username, domain)
     if user is not None:
         db.delete(user)
--- src/sql_dovecot/models.py
+++ src/sql_dovecot/models.py
@@ -1,3 +1,8 @@
+"""SQLAlchemy models for Dovecot.
+
+Classes:
+    - ImapUser: a user of the system
+"""
 import passlib.hash
 import sqlalchemy as sa
 import sqlalchemy.dialects.mysql
@@ -6,6 +11,24 @@ from . import database
 
 
 class ImapUser(database.Dovecot):
+    """A user of the dovecot system.
+
+    This class models a users table in the database.
+
+    Attributes:
+        username: the username
+        domain: the domain of the user
+        password: the password, hashed
+        home: the home directory of the user
+        uid: the user id
+        gid: the group id
+        active: whether the user is active
+
+    Functions:
+        set_password: set the password of the user
+        check_password: check a password against the user's password
+        email: get the email of the user
+    """
     __tablename__ = "users"
     username = sa.Column(
         sa.String(128, collation="ascii_bin"), nullable=False, primary_key=True
@@ -22,14 +45,42 @@ class ImapUser(database.Dovecot):
     )
 
     def set_password(self, password: str) -> None:
+        """Set the password of the user.
+
+        Hashes the password using argon2id and sets it as the user's password.
+
+        Args:
+            password: the password to set
+        """
         self.password = "{ARGON2ID}" + passlib.hash.argon2.hash(password)
 
     def check_password(self, password: str) -> bool:
+        """Check a password against the user's password.
+
+        Args:
+            password: the password to check
+
+        Returns:
+            bool: whether the password is correct
+
+        Raises:
+            Exception: if the password was not encoded by this class
+        """
         if not self.password.startswith("{ARGON2ID}"):
             raise Exception("This password was not encoded by me, i can't check it")
         return passlib.hash.argon2.verify(password, self.password[len("{ARGON2ID}") :])
 
     def email(self) -> str:
+        """Get the email of the user.
+
+        Returns:
+            str: the email of the user
+
+        Example:
+            >>> user = ImapUser(username="test", domain="example.com")
+            >>> user.email()
+
+        """
         return self.username + "@" + self.domain
 
 # CREATE TABLE `users` (
