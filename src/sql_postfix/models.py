666f140f2df344868fdf5a8eec5e1b087e076d9f
Benjamin Bayart <bbayart@ext.scaleway.com>
2024-07-24 11:44:09 +0200

    removed collation for sqlite

--- src/sql_postfix/models.py
+++ src/sql_postfix/models.py
@@ -6,22 +6,22 @@ from . import database
 class PostfixAlias(database.Postfix):
     __tablename__ = "aliases"
     alias = sa.Column(sa.String(500), nullable=False, primary_key=True)
-    domain = sa.Column(sa.String(100), nullable=False, index=True)
+    domain = sa.Column(sa.String(128), nullable=False, index=True)
     destination = sa.Column(sa.String(500), nullable=False, primary_key=True)
 
 class PostfixSender(database.Postfix):
     __tablename__ = "sender_login_map"
-    sender = sa.Column(sa.String(500, collation="ascii_bin"), nullable=False, primary_key=True)
-    login = sa.Column(sa.String(500, collation="ascii_bin"), nullable=False, primary_key=True, index=True)
-    domain = sa.Column(sa.String(128, collation="ascii_bin"), nullable=False, index=True)
+    sender = sa.Column(sa.String(500), nullable=False, primary_key=True)
+    login = sa.Column(sa.String(500), nullable=False, primary_key=True, index=True)
+    domain = sa.Column(sa.String(128), nullable=False, index=True)
 
 class PostfixMailboxDom(database.Postfix):
     __tablename__ = "mailbox_domains"
-    domain = sa.Column(sa.String(128, collation="ascii_bin"), nullable=False, primary_key=True)
-    mailbox_domain = sa.Column(sa.String(128, collation="ascii_bin"), nullable=False, index=True)
+    domain = sa.Column(sa.String(128), nullable=False, primary_key=True)
+    mailbox_domain = sa.Column(sa.String(128), nullable=False, index=True)
 
 class PostfixAliasDom(database.Postfix):
     __tablename__ = "alias_domains"
-    domain = sa.Column(sa.String(128, collation="ascii_bin"), nullable=False, primary_key=True)
-    alias_domain = sa.Column(sa.String(128, collation="ascii_bin"), nullable=False, index=True)
+    domain = sa.Column(sa.String(128), nullable=False, primary_key=True)
+    alias_domain = sa.Column(sa.String(128), nullable=False, index=True)
 
