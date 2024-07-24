5c870341262c5e0669026e653454ebe2b7bceca6
Benjamin Bayart <bbayart@ext.scaleway.com>
2024-07-24 11:00:59 +0200

    Added the tables for sender and domains in postfix database

--- src/sql_postfix/models.py
+++ src/sql_postfix/models.py
@@ -8,3 +8,20 @@ class PostfixAlias(database.Postfix):
     alias = sa.Column(sa.String(500), nullable=False, primary_key=True)
     domain = sa.Column(sa.String(100), nullable=False, index=True)
     destination = sa.Column(sa.String(500), nullable=False, primary_key=True)
+
+class PostfixSender(database.Postfix):
+    __tablename__ = "sender_login_map"
+    sender = sa.Column(sa.String(500, collation="ascii_bin"), nullable=False, primary_key=True)
+    login = sa.Column(sa.String(500, collation="ascii_bin"), nullable=False, primary_key=True, index=True)
+    domain = sa.Column(sa.String(128, collation="ascii_bin"), nullable=False, index=True)
+
+class PostfixMailboxDom(database.Postfix):
+    __tablename__ = "mailbox_domains"
+    domain = sa.Column(sa.String(128, collation="ascii_bin"), nullable=False, primary_key=True)
+    mailbox_domain = sa.Column(sa.String(128, collation="ascii_bin"), nullable=False, index=True)
+
+class PostfixAliasDom(database.Postfix):
+    __tablename__ = "alias_domains"
+    domain = sa.Column(sa.String(128, collation="ascii_bin"), nullable=False, primary_key=True)
+    alias_domain = sa.Column(sa.String(128, collation="ascii_bin"), nullable=False, index=True)
+
